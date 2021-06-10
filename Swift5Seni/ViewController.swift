8a660968a9c8d2c61209018bab37f3e22bef8824
桑原雅史 <brucemclarenp4@gmail.com>
2021-06-10 18:32:11 +0900

    view controller定義

---
+++ Swift5Seni/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5Seni
+//
+//  Created by 桑原雅史 on 2021/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
