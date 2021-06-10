b2b7cf5488ec9e7175c704d5266f4d80da26a46a
桑原雅史 <brucemclarenp4@gmail.com>
2021-06-10 19:36:39 +0900

    カウントアップ追加

--- Swift5Seni/NextViewController.swift
+++ Swift5Seni/NextViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    @IBOutlet weak var label2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
     
 
